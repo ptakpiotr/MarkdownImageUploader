ef9484424eedeb0b82424b3ee1ebf8b6190f7db1
ptakpiotr <piter020@outlook.com>
2022-01-14 17:49:39 +0100

    Initial commit

---
+++ MarkdownFileUploaderConsole/ImageUploader.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Storage.Blobs;
+using System.Text.RegularExpressions;
+
+namespace MarkdownFileUploaderConsole
+{
+    internal class ImageUploader
+    {
+        private readonly string regExp = @"\!\[.*\]\(.*(.jpg|.png)\)";
+        private readonly BlobContainerClient _client;
+
+        public ImageUploader(BlobContainerClient client)
+        {
+            _client = client;
+        }
+
+        private async Task UploadFileAsync(string path)
+        {
+            if (File.Exists(path))
+            {
+                string exts = Path.GetExtension(path);
+                string randomName = Guid.NewGuid().ToString();
+                string newName = $"{randomName}{exts}";
+                using(var stream = File.OpenRead(path))
+                {
+                    await _client.UploadBlobAsync(newName, stream);
+                }
+            }
+
+        }
+
+        public async Task SaveToBlobAsync(string txt)
+        {
+            List<string> strings = txt.Split("\\n").ToList();
+            try {
+                foreach (var str in strings)
+                {
+                    foreach (var matched in Regex.Matches(str, regExp))
+                    {
+                        string mt = matched.ToString();
+                        if (!string.IsNullOrEmpty(mt))
+                        {
+                            mt = mt.Substring(mt.IndexOf("(") + 1);
+                            mt = mt.TrimEnd(')');
+                            await UploadFileAsync(mt);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ MarkdownFileUploaderConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+using MarkdownFileUploaderConsole;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        //I am storing sensitive information like azure blob storage connection string in plain-text appsettings.json if you want more security consider using Azure Key Vault
+        //Remember to change all placeholders for valid values, otherwise this console app will not work
+
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+        string containerName = configuration["ContainerName"].ToString();
+
+
+        BlobServiceClient blobServiceClient = new BlobServiceClient(configuration.GetConnectionString("AzureBlob"));
+        var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+        string wholeText = File.ReadAllText("<YOUR-FILE-PATH>");
+
+        ImageUploader img = new(containerClient);
+        await img.SaveToBlobAsync(wholeText);
+    }
+}
